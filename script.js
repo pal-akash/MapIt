1769d14fcd2814dbbd44379d547c4e0cb1e8d79f
Akash Pal <108969268+pal-akash@users.noreply.github.com>
2024-06-25 20:20:52 +0530

    create workout class

--- script.js
+++ script.js
@@ -13,6 +13,55 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 // let map, mapEvent;
 
+class Workout {
+
+  date = new Date();
+  id = (Date.now() + '').slice(-10);
+
+  constructor(coords, distance, duration){
+    // this.date = 
+    // this.id = 
+    this.coords = coords;
+    this.distance = distance;  //km
+    this.duration = duration; // min
+  }
+}
+
+class Running extends Workout{
+  constructor(coords, distance, duration, cadence){
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+
+  calcPace(){
+    // min/km
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+
+  
+}
+
+class Cycling extends Workout{
+  constructor(coords, distance, duration, elevationGain){
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+
+  calcSpeed(){
+    // km/hr
+    this.speed = this.distance / this.duration / 60;
+    return this.speed;
+  }
+}
+
+// const run1 = new Running([39,-12], 5.2, 24, 178);
+// const cycling1 = new Cycling([39,-12], 17, 95, 523);
+// console.log(run1, cycling1);
+
+// Application architecture
 class App {
   #map;
   #mapEvent;
